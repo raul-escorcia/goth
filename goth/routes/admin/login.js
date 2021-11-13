1ec66dbb25cb8b65790c591493cd249c33c7186c
Raul Escorcia <raulescorcia@gmail.com>
2021-11-12 21:12:57 -0600

    ya funciona el boton "cerrar sesión"

--- goth/routes/admin/login.js
+++ goth/routes/admin/login.js
@@ -8,6 +8,13 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/logout', function(req, res, next){
+  req.session.destroy();
+  res.render('admin/login',{
+    layout: 'admin/layout'
+  })
+})
+
 router.post('/', async (req, res, next) => {
   try {
     var usuario = req.body.usuario;
